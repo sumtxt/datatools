8f945cdec0e181238cc65e975926e636cc1983b3
MM <moritz.marbach@gess.ethz.ch>
2018-04-18 10:46:53 +0200

    New function kill_format_tbl() to disable tibble package formatting.

---
+++ R/kill_format_tbl.R
@@ -0,0 +1,30 @@
+#' Disables all formatting of the tibble() package 
+#' 
+#' This function overrides the tibble print() function 
+#' such that calling print() on a tbl object displays 
+#' the first 10 rows in the format of a data.frame in base R. 
+#' Call after loading the tibble package. 
+#'  
+#' @examples 
+#' \dontrun{
+#' 
+#' library(tibble)
+#' 
+#' tibble(x = runif(10), y = x * 2)
+#' 
+#' kill_format_tbl()
+#' tibble(x = runif(10), y = x * 2)
+#' 
+#' } 
+#' 
+#' 
+#' 
+#' @export
+kill_format_tbl <- function() { 
+
+print.tbl_df <<- function(x, ...) {
+  print(head(as.data.frame(x),n=10), ...)
+  invisible(x)
+}
+
+}
